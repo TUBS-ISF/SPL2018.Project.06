5be264595cb608f389a735131a8e8a2d3ac44538
Lisa Rosenberg <lisa.rosenberg@outlook.de>
2018-06-10 21:40:49 +0200

    Remove java headers from files

--- Hirakanaji-Task4/features/Scrambler/HirakanajiApplication.java
+++ Hirakanaji-Task4/features/Scrambler/HirakanajiApplication.java
@@ -1,16 +1,3 @@
-package de.tubs.hirakanaji;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Stream;
-
-import static de.tubs.hirakanaji.core.RomajiDataSet.*;
-import static de.tubs.hirakanaji.core.HiraganaDataSet.*;
-import static de.tubs.hirakanaji.core.KatakanaDataSet.*;
-
 /**
  * @author lisa-rosenberg
  * @since 13.05.2018
